a6d98c7ede6f9a7a0599f6d7b279facdbfa08f59
Pritam Kap (Omnicom) <pritam.kap@interactiveavenues.com>
2026-04-09 16:12:31 +0530

    update

--- hooks/useGoogleAuth.js
+++ hooks/useGoogleAuth.js
@@ -1,22 +1,28 @@
-import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import { useEffect } from 'react';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function useGoogleAuth(onSuccess) {
-
-  const redirectUri = "https://auth.expo.io/@pritamkap123/GoogleDriveApp";
-
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: '417059671592-8hpg94oqiohctu16phppadvgpcl7b2rj.apps.googleusercontent.com',
-    redirectUri,
-
-    scopes: [
-      'https://www.googleapis.com/auth/spreadsheets',
-      'https://www.googleapis.com/auth/drive.file',
-    ],
+  const redirectUri = AuthSession.makeRedirectUri({
+    useProxy: true,
   });
+
+  const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    {
+      clientId: '417059671592-8hpg94oqiohctu16phppadvgpcl7b2rj.apps.googleusercontent.com',
+      scopes: [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive.file',
+      ],
+      redirectUri,
+    },
+    {
+      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+      tokenEndpoint: 'https://oauth2.googleapis.com/token',
+    }
+  );
 
   useEffect(() => {
     if (response?.type === 'success') {
@@ -30,35 +36,5 @@ export default function useGoogleAuth(onSuccess) {
 
   return { promptAsync, request };
 }
-import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser';
-import { useEffect } from 'react';
 
-WebBrowser.maybeCompleteAuthSession();
 
-export default function useGoogleAuth(onSuccess) {
-
-  const redirectUri = "https://auth.expo.io/@pritamkap123/GoogleDriveApp";
-
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: '417059671592-8hpg94oqiohctu16phppadvgpcl7b2rj.apps.googleusercontent.com',
-    redirectUri,
-
-    scopes: [
-      'https://www.googleapis.com/auth/spreadsheets',
-      'https://www.googleapis.com/auth/drive.file',
-    ],
-  });
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const access_token = response.authentication?.access_token;
-
-      if (access_token && onSuccess) {
-        onSuccess(access_token);
-      }
-    }
-  }, [response]);
-
-  return { promptAsync, request };
-}
